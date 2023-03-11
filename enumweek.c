98b9b9da7209de5f09e1c039ad2754e6bf061b0e
kripa-skill-lync <kripakaliyadan@skill-lync.com>
2023-03-11 15:47:47 +0530

    just submitting the enium code

---
+++ enumweek.c
@@ -0,0 +1,23 @@
+/*
+ *      enumweek.c
+ *
+
+ *      Author: KRIPA
+ */
+
+#include <stdio.h>
+
+enum week {Sunday=5, Monday=10, Tuesday, Wednesday};
+
+int main()
+{
+    // creating today variable of enum week type
+    enum week today;
+  //  enum int variable name; int a=5;int a=tuesday;
+    //today =Tuesday ;
+    today=Sunday;
+
+    printf("Day %d",today);
+
+    return 0;
+}
